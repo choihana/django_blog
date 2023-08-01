152144c83a601b9224430f83b5b557af29ed4833
Joey <hanazzang999@gmail.com>
2023-08-01 09:31:19 +0900

    profile edit view, template

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
 
@@ -58,4 +58,20 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form':user_form})
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance = request.user,
+                                 data = request.POST)
+        profile_form = ProfileEditForm(instance = request.user.profile,
+                                       data= request.POST,
+                                       files = request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request,'account/edit.html',{'user_form':user_form, 'profile_form':profile_form})
 
