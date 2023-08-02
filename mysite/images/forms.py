19d1e93bf6430bc18e9b2efade0a8a05823acd2f
Joey <hanazzang999@gmail.com>
2023-08-02 09:41:41 +0900

    image 저장 함수 save

--- mysite/images/forms.py
+++ mysite/images/forms.py
@@ -1,7 +1,8 @@
 from django.forms import forms
-
 from .models import Image
-
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -18,3 +19,15 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
+
+    def save(self, force_insert = False, force_update = False, commit = True):
+        image = super().save(commit = False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.',1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content),save=False)
+        if commit:
+            image.save()
+        return image
