277a9a253e62357122dffa7365f8b259a696cad5
Joey <hanazzang999@gmail.com>
2023-08-04 10:37:08 +0900

    follow 기능 구현

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -166,3 +168,9 @@ AUTHENTICATION_BACKENDS = [
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '565308545721-7ilpggn1ett34cqpjf10l5d0fvps5gm0.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+ABSOLUTE_URL_OVERRIDES =  {
+    'auth.user' : lambda u: reverse_lazy('user_detail', args = [u.username])
+}
+
+THUMBNAIL_DEBUG = True
